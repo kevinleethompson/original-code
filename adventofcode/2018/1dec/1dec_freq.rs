7e7965590f5a82f48722efe3a26954223311e325
Kevin Thompson <kevleethompson@gmail.com>
2018-12-13 14:05:44 -0500

    finally refactored 1dec puzzle to take 5 seconds instead of 15+ minutes to get answer

--- adventofcode/2018/1dec/1dec_freq.rs
+++ adventofcode/2018/1dec/1dec_freq.rs
@@ -5,26 +5,18 @@ use std::fs::File;
 fn main() {
     let f = File::open("1dec_input.txt").unwrap();
     let base_modulations: Vec<i32> = BufReader::new(f).lines().map(|l| l.unwrap().parse::<i32>().unwrap()).collect();
-    let first_cycle: Vec<(usize, i32)> = (&base_modulations).iter().by_ref().scan(0, |state, m| { *state = *state + m; Some(*state) }).enumerate().collect();
-    let net_change_per_cycle: i32 = (&first_cycle)[first_cycle.len() - 1].1;
+    let first_cycle: Vec<i32> = (&base_modulations).iter().by_ref().scan(0, |state, m| { *state = *state + m; Some(*state) }).collect();
+    let net_change_per_cycle: i32 = (&first_cycle)[first_cycle.len() - 1];
     println!("freq (part one): {}", &net_change_per_cycle);
-    let mut cycle = (&first_cycle).iter().cloned().cycle();
-    let mut curr_mod = 0;
-    let mut cycle_num = 1;
-    let found_repeat: i32 = 'outer: loop {
-        let unwrapped = cycle.next().unwrap();
-        if unwrapped.0 == first_cycle.len() - 1 { cycle_num += 1; }
-        curr_mod = (net_change_per_cycle * cycle_num) + unwrapped.1;
-        print!("{} ", curr_mod);
-        let mut n = cycle_num - 1;
-        while n >= 0 {
-            let found = first_cycle.iter().by_ref().rev().find_map(|m| {
-                let val = (net_change_per_cycle * n) + m.1;
-                match val == &curr_mod { true => Some(val), false => None }
-            });
-            if let Some(x) = found { break 'outer x; }
-            n = n - 1;
-        }
-    };
-    println!("repeat (part two): {}", found_repeat);
+    let mut cycle_num = 0;
+    let found = base_modulations.iter()
+                .scan(0, |state, m| { *state = *state + m; Some(*state) })
+                .enumerate()
+                .cycle()
+                .find_map(|m| {
+                    if m.0 == 0 { cycle_num += 1; }
+                    let val = (net_change_per_cycle * cycle_num) + m.1;
+                    match first_cycle.contains(&val) { true => Some(val), false => None }
+                });
+    println!("repeat (part two): {}", found.unwrap());
 }
